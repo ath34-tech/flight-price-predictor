d3bcf94d89c6b9b74cb7fa982f84b97e31747cde
ath tripathi <athtripathi@gmail.com>
2024-10-27 02:50:45 +0530

    specified port

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def predict():
                                prediction_text="Error: " + str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
